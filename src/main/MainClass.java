4a1cee7a8292b224a24b4c5e2fba69ed4f9271f1
jongmin.ryu <ryu1917@gmail.com>
2021-03-02 17:55:35 +0900

    기본프로젝트 생성

---
+++ src/main/MainClass.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) { 		
+	//kim -로그인 관련 기능
+	//cho -회원 관리 기능
+		System.out.println("main인 시작");
+	Scanner sc=new Scanner(System.in);
+	int num;
+	while(true) {
+		System.out.println("1.로그인 2.회원관리\n>>>");
+		num =sc.nextInt();
+		switch(num) {
+		case 1:
+			break;
+		case 2:
+			break;
+	
+		}
+	}
+	
+	
+	
+	
+	
+	
+	}
+}
