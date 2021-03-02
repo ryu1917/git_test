f9ef773ae3e8f92259a813854c3815ff85b28af0
jongmin.ryu <ryu1917@gmail.com>
2021-03-02 20:07:32 +0900

    로그인 기능 완성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.Scanner;
 
+import login.Login;
+
 public class MainClass {
 	public static void main(String[] args) { 		
 	//kim -로그인 관련 기능
@@ -14,7 +16,7 @@ public class MainClass {
 		num =sc.nextInt();
 		switch(num) {
 		case 1:
-			Login lo=new Login();
+			Login lo =new Login();
 			lo.login();
 			break;
 		case 2:
