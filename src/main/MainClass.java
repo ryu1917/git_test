05824a525c437c889343b999579a3d5f64491857
jongmin.ryu <ryu1917@gmail.com>
2021-03-02 19:59:05 +0900

    회원관리 기능 완성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.Scanner;
 
+import member.Member;
+
 public class MainClass {
 	public static void main(String[] args) { 		
 	//kim -로그인 관련 기능
@@ -10,12 +12,14 @@ public class MainClass {
 	Scanner sc=new Scanner(System.in);
 	int num;
 	while(true) {
-		System.out.println("1.로그인 2.회원관리\n>>>");
+		System.out.print("1.로그인 2.회원관리\n>>>");
 		num =sc.nextInt();
 		switch(num) {
 		case 1:
 			break;
 		case 2:
+			Member m =new Member();
+			m.member();
 			break;
 	
 		}
